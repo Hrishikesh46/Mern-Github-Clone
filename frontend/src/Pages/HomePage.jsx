2669f26165b73db9548bdd2b5b8af8eff2ee6adf
Hrishikesh Yadav <hrishikeshyadav92@gmail.com>
2024-05-09 18:13:12 +0530

    Getting the github API key

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -18,7 +18,14 @@ const HomePage = () => {
     async (username = "Hrishikesh46") => {
       setLoading(true);
       try {
-        const userRes = await fetch(`https://api.github.com/users/${username}`);
+        const userRes = await fetch(
+          `https://api.github.com/users/${username}`,
+          {
+            headers: {
+              authorization: `token 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`,
+            },
+          }
+        );
         const userProfile = await userRes.json();
         setUserProfile(userProfile);
 
@@ -53,10 +60,22 @@ const HomePage = () => {
     setLoading(false);
   };
 
+  const onSort = (sortType) => {
+    if (sortType === "recent") {
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, recent first
+    } else if (sortType === "stars") {
+      repos.sort((a, b) => b.stargazers_count - a.stargazers_count); // descending, most stars first
+    } else if (sortType === "forks") {
+      repos.sort((a, b) => b.forks_count - a.forks_count); //descending, most forks first
+    }
+    setSortType(sortType);
+    setRepos([...repos]);
+  };
+
   return (
     <div className="m-4">
       <Search onSearch={onSearch} />
-      {repos.length > 0 && <SortRepos setSortType={setSortType} />}
+      {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
         {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
         {!loading && <Repos repos={repos} />}
